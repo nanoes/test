f97f2736fd6f26aa4283126e7377d7916e263170
nanoes <matias.rw@hotmail.com>
2022-06-03 23:54:21 -0700

    5

---
+++ testLara/database/migrations/2022_06_04_063348_create_countries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            //relacion con person -> country
+            $table->string('country',15);//con el numero le especificamos el tamaño del campo
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
